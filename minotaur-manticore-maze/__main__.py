6b700ca131bd8d8639b11944c29ab1515ee0461d
Shane McGuckian <shane.mcguckian@gmail.com>
2018-06-30 14:15:02 -0600

    modified __main__.py to utilize LoadMap and Bounds classes

--- minotaur-manticore-maze/__main__.py
+++ minotaur-manticore-maze/__main__.py
@@ -1,4 +1,9 @@
 from load_map import LoadMap
+from bounds import Bounds
+# from navigation import Navigation
 
-map = LoadMap('map.csv')
-print(map.df)
+map = LoadMap('map.csv', start_row=4, start_col=0)
+current_loc = map.start_point
+
+bounds = Bounds(current_loc).interpret()
+print(bounds)
