7c6ad3dd7fb3b0188b1fca849bdccb9121647ee2
Shane McGuckian <shane.mcguckian@gmail.com>
2018-06-30 14:18:33 -0600

    modified LoadMap to set start_point instead of seting start_point in Navigation class

--- minotaur-manticore-maze/load_map.py
+++ minotaur-manticore-maze/load_map.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 class LoadMap():
 
-    def __init__(self, map_csv):
-        self.map_csv = map_csv
+    def __init__(self, map_csv, start_row, start_col):
         self.df = pd.read_csv(map_csv, header=None)
+        start_col_list = self.df.iloc[:, start_col].str.split(' ').tolist()
+        self.start_point = start_col_list[start_row]
