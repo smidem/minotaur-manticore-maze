dc7331c0135d639c37efb16d57f413d673542d8f
Shane McGuckian <shane.mcguckian@gmail.com>
2018-06-30 16:09:34 -0600

    created strings.py in order remove excessive text from several classes

---
+++ minotaur-manticore-maze/strings.py
@@ -0,0 +1,20 @@
+from textwrap import dedent
+
+
+class ItemStrings():
+
+    def __init__(self):
+
+        self.compass_part_1 = dedent("""
+            As you begin to move, you foot collides with something solid,
+            and it produces a clink sound. You bend down and spot the
+            smallest glint of something metallic. You slowly brush the
+            dirt away...\n""")
+
+        self.compass_part_2 = dedent("""
+            COMPASS ACQUIRED!
+
+            Conveniently, the compass indicates that the passage directly in
+            front you is pointed north.\n""")
+
+        self.
