7278c45e3e59bf2533471984b69d7e438f35d9f4
Shane McGuckian <shane.mcguckian@gmail.com>
2018-06-30 16:10:31 -0600

    shortened bounds.py using any() instead of for loops

--- minotaur-manticore-maze/bounds.py
+++ minotaur-manticore-maze/bounds.py
@@ -5,9 +5,8 @@ class Bounds():
         and_index = 0
         length = len(current_loc)
 
-        for i, word in enumerate(current_loc):
-            if 'and' in word:
-                and_index = i
+        if any('and' in word for word in current_loc):
+            and_index = current_loc.index('and')
 
         if and_index > 0 and and_index + 1 < length:
             self.bounds = current_loc[and_index + 1: length]
@@ -15,24 +14,20 @@ class Bounds():
             self.bounds = current_loc
 
     def interpret(self):
-        length = len(self.bounds)
-        type = ''
-        restricted = []
-
-        if length > 1:
-            type = self.bounds.pop(-1)
+        blocked = []
+        type = self.bounds.pop(-1)
 
         if 'corner' in type or 'wall' in type:
-            restricted = self.bounds
+            blocked = self.bounds
         elif 'hall' in type:
             if 'east' in self.bounds[0] and 'west' in self.bounds[1]:
-                restricted = ['north', 'south']
+                blocked = ['north', 'south']
             elif 'north' in self.bounds[0] and 'south' in self.bounds[1]:
-                restricted = ['east', 'west']
+                blocked = ['east', 'west']
         elif 'end' in type:
             if 'west' in self.bounds:
-                restricted = ['north', 'south', 'east']
+                blocked = ['north', 'south', 'east']
             elif 'east' in self.bounds:
-                restricted = ['north', 'south', 'west']
+                blocked = ['north', 'south', 'west']
 
-        return restricted
+        return blocked
